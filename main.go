486637f50b3d6327149f56cd92e4923eca5b7800
lance547 <437653103@qq.com>
2023-12-04 23:58:40 +0800

    flagImport

--- main.go
+++ main.go
@@ -1,1 +1,14 @@
-package simpleCliApplication
+package main
+
+import (
+	"flag"
+	"log"
+)
+
+func main() {
+	var name string
+	flag.StringVar(&name, "name", "lance", "用户名")
+	flag.StringVar(&name, "n", "lance", "用户名")
+	flag.Parse()
+	log.Printf("name: %s", name)
+}
